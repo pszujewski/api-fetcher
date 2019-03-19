7eed43b70cd94489d7e9ca7b2838bd0027c171ae
Peter Szujewski <peter@abovethetreeline.com>
2019-03-19 15:21:28 -0400

    Adds first set of tests for entry class

--- __tests__/ApiFetcher.test.js
+++ __tests__/ApiFetcher.test.js
@@ -1,25 +1,62 @@
 import { ApiFetcher } from '../src/ApiFetcher';
 
-function fetchMock() {
+function fetchMock(resolveWith) {
+	const defaultResolve = { id: '123', message: 'success' };
 	global.fetch = jest.fn().mockImplementation(() => {
-		const p = new Promise(resolve => {
+		return new Promise(resolve => {
 			resolve({
 				ok: true,
-				json: () => ({ id: '123', message: 'success' }),
+				json: () => (resolveWith ? resolveWith : defaultResolve),
 			});
 		});
-		return p;
 	});
 }
 
-beforeEach(() => {
-	fetchMock();
-});
+const setupFetcher = () => {
+	const prefix = 'http://hello.com/api';
+	const fetcher = new ApiFetcher(prefix);
+	return fetcher;
+};
 
 describe('ApiFetcher', () => {
 	it('Should set a urlPrefix for all calls', () => {
-		const prefix = 'http://hello.com/api';
-		const fetcher = new ApiFetcher(prefix);
-		expect(fetcher.api.urlPrefix).toBe(prefix);
+		const fetcher = setupFetcher();
+		expect(fetcher.api.urlPrefix).toBe('http://hello.com/api');
+	});
+
+	it('Should set options for fetch if given by user', () => {
+		const fetcher = setupFetcher();
+		const testOptions = {
+			mode: 'cors',
+			cache: 'no-cache',
+			credentials: 'same-origin',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			redirect: 'follow',
+			referrer: 'no-referrer',
+		};
+
+		fetcher.setFetchOptions(testOptions);
+		expect(fetcher.api.fetchOptions).toEqual(testOptions);
+	});
+
+	it('Should return a response Promise for .get() method', () => {
+		fetchMock();
+		const fetcher = setupFetcher();
+
+		expect.assertions(1);
+		return fetcher.get('/123').then(data => {
+			return expect(data).toEqual({ id: '123', message: 'success' });
+		});
+	});
+
+	it('Should return a wrapped Promise for .cancelableGet()', () => {
+		fetchMock({ id: '345' });
+		const fetcher = setupFetcher();
+
+		const { promise, cancel } = fetcher.cancelableGet('/123');
+		expect(typeof cancel).toBe('function');
+		expect(promise).toBeInstanceOf(Promise);
 	});
 });
